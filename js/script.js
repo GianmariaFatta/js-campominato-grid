d7f8e498db711b18b7f9454e0ead3ee77677f332
GianmariaFatta <gianmaria.fatta@gmail.com>
2022-12-16 19:17:58 +0100

    end commit

--- js/script.js
+++ js/script.js
@@ -16,3 +16,49 @@
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+const buttonElement=document.getElementById("button")
+const gridElement=document.getElementById("grid")
+const cellElement=document.getElementsByClassName("cell")
+const titleElement=document.getElementById("title")
+
+
+const rows=10;
+const cols=10;
+const totalCells= rows * cols
+// FUNZIONI------------------
+const createCell =(content) => {
+    const cell = document.createElement("div");
+    cell.append(content);
+    cell.classList.add("cell");
+    gridElement.appendChild(cell);
+    return cell;
+}
+   
+    
+let j=0
+//generiamo azione al click del bottone
+
+buttonElement.addEventListener("click", function(){
+    
+    
+    for(let i=1; i<=totalCells;i++){
+        titleElement.classList.add("d-none");
+      const cell=  createCell(i) ;
+      
+      
+      cell.addEventListener("click",function() {
+        cell.classList.toggle("checked");
+        console.log(i)
+        
+    })
+    }
+    
+    })
+
+
+    
+    
+    
+
+    
